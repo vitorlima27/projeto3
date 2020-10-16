ce940a62ff2e66ce22a58a13c852600498054018
João Vitor de Lima <joao-vitordelima99@hotmail.com>
2020-10-15 22:59:05 -0300

    Add files via upload

---
+++ frontend/src/components/inicio/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+<Main icon="home" title="Início" subtitle="Cadastro de caronas" >
+            <div className="display-4">
+                Seja bem Vindo ao Vumbora, seu site de caronas!
+            </div>
+            <hr/>
+            <p className="mb-0">Sistema de Cadastro de Caronas</p>
+            
+</Main>
